336762ba4898713be6d4cea0a01c3adc3fc0c75d
Hayden A. James <hayden.james@gmail.com>
2025-12-22 21:40:26 -0500

    Change to executable permissions on binary during setup

--- Formula/grpcwebcurl.rb
+++ Formula/grpcwebcurl.rb
@@ -5,24 +5,22 @@ class Grpcwebcurl < Formula
   license "Apache-2.0"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/hjames9/grpcwebcurl/releases/download/v0.2.1/grpcwebcurl-v0.2.1-darwin-arm64"
       sha256 "61b8653cb2e889100807e63d62574c1622f1e421998d1d4dae8b0a77ec17e961"
-    else
+    end
+
+    on_intel do
       url "https://github.com/hjames9/grpcwebcurl/releases/download/v0.2.1/grpcwebcurl-v0.2.1-darwin-amd64"
       sha256 "3ccacd13b189efc6261351784d0e66fe8b64931db766843abd0fd76840fe5c90"
     end
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "grpcwebcurl-v#{version}-darwin-arm64" => "grpcwebcurl"
-    else
-      bin.install "grpcwebcurl-v#{version}-darwin-amd64" => "grpcwebcurl"
-    end
-
-    # Generate shell completions
-    generate_completions_from_executable(bin/"grpcwebcurl", "completion")
+    binary_name = "grpcwebcurl-v#{version}-darwin-#{Hardware::CPU.arch.to_s.sub("x86_64", "amd64")}"
+    chmod 0755, binary_name
+    bin.install binary_name => "grpcwebcurl"
+    generate_completions_from_executable(bin/"grpcwebcurl", "completion", shells: [:bash, :zsh, :fish])
   end
 
   test do
